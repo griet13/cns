8e559fcc1de13e532dae9eab7098a08b8b0de1ba
demo <djangouser7075@gmail.com>
2026-08-27 19:43:13 +0530

    Implement Diffie-Hellman key exchange in Java

---
+++ Task 8.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.math.*;
+public class DiffieHellman{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter prime number:");
+        BigInteger p=new BigInteger(br.readLine());
+        System.out.print("Enter primitive root of " + p + ":");
+        BigInteger g=new BigInteger(br.readLine());
+        System.out.println("Enter value for x less than " + p + ":");
+        BigInteger x=new BigInteger(br.readLine());
+        BigInteger R1=g.modPow(x,p);
+        System.out.println("R1=" + R1);
+        System.out.print("Enter value for y less than " + p + ":");
+        BigInteger y=new BigInteger(br.readLine());
+        BigInteger R2=g.modPow(y,p);
+        System.out.println("R2=" + R2);
+        BigInteger k1=R2.modPow(x,p);
+        System.out.println("Key1 calculated: " + k1);
+        BigInteger k2=R1.modPow(y,p);
+        System.out.println("Key2 calculated: " + k2);
+        System.out.println("Diffie Hellman secret key encryption has taken");
+    }
+}
