dac04da829ea4be29960e32a5e003e7e1cd58fc7
demo <djangouser7075@gmail.com>
2026-08-27 19:25:13 +0530

    Add DES encryption and decryption functionality
    Implement encryption and decryption using DES algorithm.

---
+++ Task 2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import javax.crypto.*;
+public class Task2{
+    static Scanner s=new Scanner(System.in);
+    static Cipher cipher;
+    static byte[] encrypt(String data) throws Exception{
+        byte[] encrypted=cipher.doFinal(data.getBytes());
+        System.out.println("Encrypted: " + Arrays.toString(encrypted));
+        return encrypted;
+    }
+    static void decrypt(byte[] data) throws Exception{
+        byte[] decrypted=cipher.doFinal(data);
+        System.out.println("Decrypted: " + new String(decrypted));
+    }
+    public static void main(String[] args){
+        try{
+            String data=s.nextLine();
+            KeyGenerator kg=KeyGenerator.getInstance("DES");
+            SecretKey key=kg.generateKey();
+            cipher=Cipher.getInstance("DES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE,key);
+            byte[] encrypted=encrypt(data);
+            cipher.init(Cipher.DECRYPT_MODE,key);
+            decrypt(encrypted);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
