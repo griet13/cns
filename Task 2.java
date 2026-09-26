7bba592928798af43daa946b3e283de1c03586f1
demo <djangouser7075@gmail.com>
2026-09-26 13:21:26 +0530

    Refactor Task2 class to Main and improve code structure

--- Task 2.java
+++ Task 2.java
@@ -1,28 +1,30 @@
 import java.util.*;
 import javax.crypto.*;
-public class Task2{
-    static Scanner s=new Scanner(System.in);
+public class Main{
+    static Scanner sc = new Scanner(System.in);
     static Cipher cipher;
     static byte[] encrypt(String data) throws Exception{
-        byte[] encrypted=cipher.doFinal(data.getBytes());
+        byte[] encrypted = cipher.doFinal(data.getBytes());
         System.out.println("Encrypted: " + encrypted);
         return encrypted;
     }
-    static void decrypt(byte[] data) throws Exception{
-        byte[] decrypted=cipher.doFinal(data);
+    static byte[] decrypt(byte[] data) throws Exception{
+        byte[] decrypted = cipher.doFinal(data);
         System.out.println("Decrypted: " + new String(decrypted));
+        return decrypted;
     }
     public static void main(String[] args){
         try{
-            String data=s.nextLine();
-            KeyGenerator kg=KeyGenerator.getInstance("DES");
-            SecretKey key=kg.generateKey();
-            cipher=Cipher.getInstance("DES/ECB/PKCS5Padding");
+            String data = sc.nextLine();
+            KeyGenerator kg = KeyGenerator.getInstance("DES");
+            SecretKey key = kg.generateKey();
+            cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE,key);
-            byte[] encrypted=encrypt(data);
+            byte[] encrypted = encrypt(data);
             cipher.init(Cipher.DECRYPT_MODE,key);
             decrypt(encrypted);
-        } catch(Exception e){
+        }
+        catch(Exception e){
             e.printStackTrace();
         }
     }
