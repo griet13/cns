f1d7ab41e96df86768821c18f1d5a4fb9a256831
demo <djangouser7075@gmail.com>
2026-09-26 13:26:58 +0530

    Refactor encryption and decryption methods

--- Task 3.java
+++ Task 3.java
@@ -1,29 +1,30 @@
 import java.util.*;
 import javax.crypto.*;
 public class Task3{
-    static Scanner s=new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
     static Cipher cipher;
-    static byte[] encrypt(String data) throws Exception{
-        byte[] encrypted=cipher.doFinal(data.getBytes());
-        System.out.println(encrypted);
+    private static byte[] encrypt(String data) throws Exception{
+        byte[] encrypted = cipher.doFinal(data.getBytes());
+        System.out.println("Encrypted: " + encrypted);
         return encrypted;
     }
-    static byte[] decrypt(byte[] data) throws Exception{
-        byte[] decrypted=cipher.doFinal(data);
-        System.out.println(new String(decrypted));
+    private static byte[] decrypt(byte[] data) throws Exception{
+        byte[] decrypted = cipher.doFinal(data);
+        System.out.println("Decrypted: " + new String(decrypted));
         return decrypted;
     }
     public static void main(String[] args){
         try{
-            String data=s.nextLine();
-            KeyGenerator keygen=KeyGenerator.getInstance("Blowfish");
-            SecretKey key=keygen.generateKey();
-            cipher=Cipher.getInstance("Blowfish");
+            String data = sc.nextLine();
+            KeyGenerator kg = KeyGenerator.getInstance("Blowfish");
+            SecretKey key = kg.generateKey();
+            cipher = Cipher.getInstance("Blowfish");
             cipher.init(Cipher.ENCRYPT_MODE,key);
-            byte[] encrypted=encrypt(data);
+            byte[] encrypted = encrypt(data);
             cipher.init(Cipher.DECRYPT_MODE,key);
             decrypt(encrypted);
-        } catch(Exception e){
+        }
+        catch(Exception e){
             e.printStackTrace();
         }
     }
